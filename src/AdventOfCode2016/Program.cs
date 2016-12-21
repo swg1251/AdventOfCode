8eb7994f9d1021da605dd914de17fe9827357074
goodm138 <goodm138@umn.edu>
2016-12-20 19:27:48 -0600

    Completed through day 5

--- src/AdventOfCode2016/Program.cs
+++ src/AdventOfCode2016/Program.cs
@@ -19,28 +19,36 @@ namespace AdventOfCode2016
 			if (!int.TryParse(dayInput, out day) || day < 1 || day > 25)
 			{
 				Console.WriteLine("Invalid day. Please enter a value between 1-25.");
-				Startup();
 			}
-
-			switch (day)
+			else
 			{
-				case 1:
-					var day01 = new Day01.Day01();
-					day01.Go();
-					break;
-				case 2:
-					var day02 = new Day02.Day02();
-					day02.Go();
-					break;
-				case 3:
-					var day03 = new Day03.Day03();
-					day03.Go();
-					break;
-				default:
-					Console.WriteLine("Not yet implemented.");
-					break;
+				switch (day)
+				{
+					case 1:
+						var day01 = new Day01.Day01();
+						day01.Go();
+						break;
+					case 2:
+						var day02 = new Day02.Day02();
+						day02.Go();
+						break;
+					case 3:
+						var day03 = new Day03.Day03();
+						day03.Go();
+						break;
+					case 4:
+						var day04 = new Day04.Day04();
+						day04.Go();
+						break;
+					case 5:
+						var day05 = new Day05.Day05();
+						day05.Go();
+						break;
+					default:
+						Console.WriteLine("Not yet implemented.");
+						break;
+				}
 			}
-
 			Startup();
 		}
     }
