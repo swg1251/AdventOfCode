7a4c012382d272c005523edafdb1b5a489814d40
Scott Goodman <scott.goodman@protolabs.com>
2017-12-11 16:10:56 -0600

    Started 2017 11-1 but approach won't work. Scrapping...

---
+++ src/AdventOfCode/2017/Day11.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Year2017
+{
+    public class Day11 : IDay
+    {
+		private Hexagon hexagon;
+
+		public void GetInput()
+		{
+			var i = 0;
+			hexagon = new Hexagon { Id = i };
+
+			var input = File.ReadAllLines("2017/input/day11.txt").Where(l => !string.IsNullOrEmpty(l)).First();
+			
+			foreach (var direction in input.Split(','))
+			{
+				i++;
+
+				if (direction == "n")
+				{
+					if (hexagon.North == null)
+					{
+						hexagon.North = new Hexagon { Id = i, South = hexagon };
+					}
+					hexagon = hexagon.North;
+				}
+				else if (direction == "ne")
+				{
+					if (hexagon.NorthEast == null)
+					{
+						hexagon.NorthEast = new Hexagon { Id = i, SouthWest = hexagon };
+					}
+					hexagon = hexagon.NorthEast;
+				}
+				else if (direction == "se")
+				{
+					if (hexagon.SouthEast == null)
+					{
+						hexagon.SouthEast = new Hexagon { Id = i, NorthWest = hexagon };
+					}
+					hexagon = hexagon.SouthEast;
+				}
+				else if (direction == "s")
+				{
+					if (hexagon.South == null)
+					{
+						hexagon.South = new Hexagon { Id = i, North = hexagon };
+					}
+					hexagon = hexagon.South;
+				}
+				else if (direction == "sw")
+				{
+					if (hexagon.SouthWest == null)
+					{
+						hexagon.SouthWest = new Hexagon { Id = i, NorthEast = hexagon };
+					}
+					hexagon = hexagon.SouthWest;
+				}
+				else if (direction == "nw")
+				{
+					if (hexagon.NorthWest == null)
+					{
+						hexagon.NorthWest = new Hexagon { Id = i, SouthEast = hexagon };
+					}
+					hexagon = hexagon.NorthWest;
+				}
+			}
+		}
+
+		public void Solve()
+		{
+			var queue = new Queue<Hexagon>();
+			queue.Enqueue(hexagon);
+
+			while (queue.Any())
+			{
+				var currentHexagon = queue.Dequeue();
+
+				if (currentHexagon.Id == 0)
+				{
+					Console.WriteLine("reached the goal");
+					return;
+				}
+
+				foreach(var neighbor in currentHexagon.GetNeighbors())
+				{
+
+				}
+			}
+		}
+
+		internal class Hexagon
+		{
+			public int Id { get; set; }
+			public Hexagon North { get; set; }
+			public Hexagon NorthEast { get; set; }
+			public Hexagon SouthEast { get; set; }
+			public Hexagon South { get; set; }
+			public Hexagon SouthWest { get; set; }
+			public Hexagon NorthWest { get; set; }
+
+			public List<int> Explored { get; set; }
+
+			public IEnumerable<Hexagon> GetNeighbors()
+			{
+
+			}
+
+			private Hexagon TryGetNeighborWithPath()
+			{
+
+			}
+		}
+    }
+}
