d5b0978b08d0de5dd51fc3f5a24b38673069ec96
Scott Goodman <scott.goodman@protolabs.com>
2018-12-14 14:10:20 -0600

    2015 day 20 done

---
+++ AdventOfCode.Tests/2015/Day20Test.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace AdventOfCode.Tests.Year2015
+{
+    public class Day20Test
+    {
+		[Fact]
+		void Part_one_test()
+		{
+			var day20 = new AdventOfCode.Year2015.Day20();
+			Assert.Equal(10, day20.GetPresentCount(1));
+			Assert.Equal(30, day20.GetPresentCount(2));
+			Assert.Equal(40, day20.GetPresentCount(3));
+			Assert.Equal(70, day20.GetPresentCount(4));
+			Assert.Equal(60, day20.GetPresentCount(5));
+			Assert.Equal(120, day20.GetPresentCount(6));
+			Assert.Equal(80, day20.GetPresentCount(7));
+			Assert.Equal(150, day20.GetPresentCount(8));
+			Assert.Equal(130, day20.GetPresentCount(9));
+		}
+    }
+}
---
+++ AdventOfCode/2015/Day20.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Year2015
+{
+	public class Day20 : IDay
+    {
+		private int input;
+
+		public void GetInput()
+		{
+			input = Convert.ToInt32(File.ReadAllLines("2015/input/day20.txt").Where(l => !string.IsNullOrEmpty(l)).First());
+		}
+
+		public void Solve()
+		{
+			var partOne = GetHouse(input);
+			Console.WriteLine($"The first house to get at least {input} presents (part one) is: {partOne}");
+
+			var partTwo = GetHouse(input, true);
+			Console.WriteLine($"The first house to get at least {input} presents (part one) is: {partTwo}");
+		}
+
+		public int GetHouse(int presentTarget, bool partTwo = false)
+		{
+			var houseNumber = 2;
+			while (GetPresentCount(houseNumber, partTwo) <= presentTarget)
+			{
+				// only consider even numbers, they have more factors
+				houseNumber+= 2;
+			}
+			return houseNumber;
+		}
+
+		public int GetPresentCount(int houseNumber, bool partTwo = false)
+		{
+			// the elves who deliver to a given house are those whose numbers are a factor of that house number
+			var deliverers = GetFactors(houseNumber);
+
+			if (partTwo)
+			{
+				// if elfNumber / houseNumber > 50, that elf has already delivered 50 presents and won't deliver to this house
+				deliverers = deliverers.Where(d => houseNumber / d <= 50);
+			}
+
+			// the number of presents is the sum of the factors (elves' numbers) times the number each elf delivers
+			return deliverers.Sum() * (partTwo ? 11 : 10);
+		}
+
+		private IEnumerable<int> GetFactors(int n)
+		{
+			var factors = new HashSet<int>();
+			for (int i = 1; i <= Math.Sqrt(n); i++)
+			{
+				if (n % i == 0)
+				{
+					factors.Add(i);
+					factors.Add(n / i);
+				}
+			}
+			return factors;
+		}
+	}
+}
