3b527286769354b02afd708b8f3d83a09799cfee
Scott Goodman <scott.goodman@protolabs.com>
2018-12-17 16:14:53 -0600

    2018 day 14 done but part two takes like 10 seconds :(

---
+++ AdventOfCode.Tests/2018/Day14Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Tests.Year2018
+{
+    public class Day14Test
+    {
+		[Fact(Skip = "Very slow due to inefficient part two")]
+		void Part_one_example()
+		{
+			var day14 = new AdventOfCode.Year2018.Day14();
+			var answer = day14.GetScore(9);
+			Assert.Equal("5158916779", answer.partOne);
+		}
+
+		[Fact(Skip = "Very slow due to inefficient part two")]
+		void Part_one_example_two()
+		{
+			var day14 = new AdventOfCode.Year2018.Day14();
+			var answer = day14.GetScore(5);
+			Assert.Equal("0124515891", answer.partOne);
+		}
+
+		[Fact(Skip = "Very slow due to inefficient part two")]
+		void Part_one_example_three()
+		{
+			var day14 = new AdventOfCode.Year2018.Day14();
+			var answer = day14.GetScore(18);
+			Assert.Equal("9251071085", answer.partOne);
+		}
+
+		[Fact(Skip = "Very slow due to inefficient part two")]
+		void Part_one_example_four()
+		{
+			var day14 = new AdventOfCode.Year2018.Day14();
+			var answer = day14.GetScore(2018);
+			Assert.Equal("5941429882", answer.partOne);
+		}
+	}
+}
---
+++ AdventOfCode/2018/Day14.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Year2018
+{
+	public class Day14 : IDay
+	{
+		private int input;
+
+		public void GetInput()
+		{
+			input = Convert.ToInt32(File.ReadAllLines("2018/input/day14.txt").Where(l => !string.IsNullOrEmpty(l)).First());
+		}
+
+		public void Solve()
+		{
+			var answer = GetScore(input);
+			Console.WriteLine($"The scores of the next ten recipes after {input} (part one) is: {answer.partOne}");
+			Console.WriteLine($"{input} first appears after (part two) {answer.partTwo} recipes");
+		}
+
+		public (string partOne, int partTwo) GetScore(int recipeCount)
+		{
+			var scores = new List<int> { 3, 7 };
+			var elf1 = 0;
+			var elf2 = 1;
+			var partTwo = -1;
+
+			while (true)
+			{
+				for (int i = 0; i < 50000000; i++)
+				{
+					var score = scores[elf1] + scores[elf2];
+					foreach (var c in score.ToString())
+					{
+						scores.Add(Convert.ToInt32(c.ToString()));
+					}
+
+					elf1 = (1 + elf1 + scores[elf1]) % scores.Count;
+					elf2 = (1 + elf2 + scores[elf2]) % scores.Count;
+				}
+
+				// check if we have part two answer yet
+				// only do this every 5 million, it's very expensive
+				partTwo = string.Join("", scores).IndexOf(recipeCount.ToString());
+				if (partTwo != -1)
+				{
+					break;
+				}
+			}
+
+			var finalScore = "";
+			for (int i = recipeCount; i < recipeCount + 10; i++)
+			{
+				finalScore += scores[i].ToString();
+			}
+
+			var scoreString = string.Join("", scores);
+			return (finalScore, partTwo);
+		}
+	}
+}
