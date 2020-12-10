71d7a5a6137a8a7eb1c7e9fd4bf03656b1906ff3
Scott Goodman <swg1251@gmail.com>
2020-12-10 11:17:37 -0600

    2020 day 10 done

---
+++ AdventOfCode/2020/Day10.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Year2020
+{
+	public class Day10 : IDay
+	{
+		private List<int> adapters;
+
+		public void GetInput()
+		{
+			adapters = File.ReadAllLines("2020/input/day10.txt")
+				.Where(l => !string.IsNullOrEmpty(l))
+				.Select(l => Convert.ToInt32(l))
+				.ToList();
+		}
+
+		public void Solve()
+		{
+			adapters.Add(0);
+			adapters.Sort();
+			adapters.Add(adapters.Max() + 3);
+
+			PartOne();
+			PartTwo();
+		}
+
+		private void PartOne()
+		{
+			var oneDiffs = 0;
+			var threeDiffs = 0;
+
+			for (int i = 0; i < adapters.Count - 1; i++)
+			{
+				var diff = adapters[i + 1] - adapters[i];
+				if (diff == 1)
+				{
+					oneDiffs++;
+				}
+				else if (diff == 3)
+				{
+					threeDiffs++;
+				}
+			}
+
+			Console.WriteLine($"The product of number of one jumps and three jumps (part one) is {oneDiffs * threeDiffs}");
+		}
+
+		private void PartTwo()
+		{
+			var pathDict = new Dictionary<int, long>();
+			foreach (var adapter in adapters)
+			{
+				// initialize with no paths to any adapter
+				pathDict[adapter] = 0;
+			}
+
+			// one path to starting adapter (0)
+			pathDict[0] = 1;
+
+			foreach (var adapter in adapters)
+			{
+				for (int i = 1; i < 4; i++)
+				{
+					// current adapter minus 1/2/3 existing means there is another adapter that paths to this
+					if (adapters.Contains(adapter - i))
+					{
+						// number of paths to this adapter += all paths to previous
+						pathDict[adapter] += pathDict[adapter - i];
+					}
+				}
+			}
+
+			Console.WriteLine($"The total number of valid combinations (part two) is {pathDict[adapters.Max()]}");
+		}
+	}
+}
