ac76691798fc5facb6f45492029addbe05a7cb03
The-G-Scott <swg1251@gmail.com>
2022-12-01 09:48:24 -0600

    2022 day 1 refactor

--- AdventOfCode/2022/Day01.cs
+++ AdventOfCode/2022/Day01.cs
@@ -31,12 +31,9 @@ namespace AdventOfCode.Year2022
 
 		public void Solve()
 		{
-			Console.WriteLine($"The elf carrying the most calories (part one) has {elves.Max(e => e.Sum())}");
-
 			var topThreeElves = elves.OrderByDescending(e => e.Sum()).Take(3);
-			var topThreeTotal = topThreeElves.Sum(e => e.Sum());
-
-			Console.WriteLine($"The amount of calories carried by the top three elves (part two) is {topThreeTotal}");
+			Console.WriteLine($"The elf carrying the most calories (part one) has {topThreeElves.First().Sum()}");
+			Console.WriteLine($"The amount of calories carried by the top three elves (part two) is {topThreeElves.Sum(e => e.Sum())}");
 		}
 	}
 }
