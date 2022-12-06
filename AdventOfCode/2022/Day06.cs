92cf398b2434c65e61be53e075c9668dd89fcb73
The-G-Scott <swg1251@gmail.com>
2022-12-06 14:35:57 -0600

    2022 day 6 done

---
+++ AdventOfCode/2022/Day06.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Year2022
+{
+	public class Day06 : IDay
+	{
+		private string input;
+
+		public void GetInput()
+		{
+			input = InputHelper.GetStringsFromInput(2022, 6).Single();
+		}
+
+		public void Solve()
+		{
+			Console.WriteLine($"The first start-of-packet marker (part one) occurs after {GetFirstMarkerIndex(input, 4)} characters");
+			Console.WriteLine($"The first start-of-message marker (part two) occurs after {GetFirstMarkerIndex(input, 14)} characters");
+		}
+
+		private int GetFirstMarkerIndex(string datastream, int markerLength)
+		{
+			for (int i = 0; i < datastream.Length + markerLength; i++)
+			{
+				var potentialMarker = datastream.Substring(i, markerLength);
+				if (potentialMarker.Distinct().Count() == markerLength)
+				{
+					return i + markerLength;
+				}
+			}
+			throw new Exception($"Failed to find marker of length {markerLength}");
+		}
+	}
+}
