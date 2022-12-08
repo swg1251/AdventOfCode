3237cadabae95fcd38ab16250ca0048be6ad2305
The-G-Scott <swg1251@gmail.com>
2022-12-08 17:11:09 -0600

    Break out file name into methods in InputHelper Add GetIntegerGridFromInput

--- AdventOfCode/InputHelper.cs
+++ AdventOfCode/InputHelper.cs
@@ -9,34 +9,47 @@ namespace AdventOfCode
 	{
 		public static List<int> GetIntegersFromInput(int year, int day)
 		{
-			var dayStr = day > 9 ? $"day{day}" : $"day0{day}";
-			var lines = File.ReadAllLines($"{year}/input/{dayStr}.txt").Where(l => !string.IsNullOrEmpty(l));
+			var lines = File.ReadAllLines(GetFileName(year, day)).Where(l => !string.IsNullOrEmpty(l));
 			return lines.Select(l => Convert.ToInt32(l)).ToList();
+		}
+
+		public static List<List<int>> GetIntegerGridFromInput(int year, int day)
+		{
+			var lines = File.ReadAllLines(GetFileName(year, day)).Where(l => !string.IsNullOrEmpty(l));
+			var grid = new List<List<int>>();
+			foreach (var line in lines)
+			{
+				grid.Add(line.Select(x => Convert.ToInt32(x)).ToList());
+			}
+			return grid;
 		}
 
 		public static List<int> GetIntegersFromCommaSeparatedInput(int year, int day)
 		{
-			var dayStr = day > 9 ? $"day{day}" : $"day0{day}";
-			var line = File.ReadAllLines($"{year}/input/{dayStr}.txt").First();
+			var line = File.ReadAllLines(GetFileName(year, day)).First();
 			return line.Split(',').Select(l => Convert.ToInt32(l)).ToList();
 		}
 
 		public static List<long> GetLongIntegersFromInput(int year, int day)
 		{
-			var dayStr = day > 9 ? $"day{day}" : $"day0{day}";
-			var lines = File.ReadAllLines($"{year}/input/{dayStr}.txt").Where(l => !string.IsNullOrEmpty(l));
+			var lines = File.ReadAllLines(GetFileName(year, day)).Where(l => !string.IsNullOrEmpty(l));
 			return lines.Select(l => Convert.ToInt64(l)).ToList();
 		}
 
 		public static List<string> GetStringsFromInput(int year, int day, bool keepBlankLines = false)
 		{
-			var dayStr = day > 9 ? $"day{day}" : $"day0{day}";
-			var lines = File.ReadAllLines($"{year}/input/{dayStr}.txt").ToList();
+			var lines = File.ReadAllLines(GetFileName(year, day)).ToList();
 			if (!keepBlankLines)
 			{
 				lines = lines.Where(l => !string.IsNullOrEmpty(l)).ToList();
 			}
 			return lines;
+		}
+
+		private static string GetFileName(int year, int day)
+		{
+			var dayStr = day > 9 ? $"day{day}" : $"day0{day}";
+			return $"{year}/input/{dayStr}.txt";
 		}
 	}
 }
